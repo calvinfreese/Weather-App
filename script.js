ad427c2118d7e69c41ae2346a1965f7407fbe39f
whackingMUFN <whackingmuffin3@hotmail.com>
2020-08-11 20:49:20 -0500

    keys keys keys

--- script.js
+++ script.js
@@ -177,7 +177,10 @@ function appendHistory() {
   // var button = $('button');
   listGroup.empty();
   var keys = Object.keys(localStorage);
-  var keysIndex = keys[keys.length -1].split("_")[1];
+  if (keys != null) {
+    var keysIndex = keys[keys.length - 1].split("_")[1];
+  }
+  
    for (i = 0; i < history.length; i++) {
     listGroup.prepend(
       `<li class='list-group-item list-group-item-action historical-search'> ${history[i]} </li>`
